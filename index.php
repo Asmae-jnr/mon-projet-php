f38824ebce832a7e10b36aea1bf73c534e086d67
Asmae Laaroussi <asmaelaaroussi221@gmail.com>
2024-09-07 14:18:28 +0100

    formulaire

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form>
+        <label>Username :</label><br>
+        <input type="text" name="username"><br>
+        <label>Password :</label><br>
+        <input type="text" name="pasword"><br>
+        <input type="submit" value="Log In">
+    </form>
+</body>
+</html>
 <?php
 echo "Bonjour, bienvenue sur mon projet PHP !";
 ?>
